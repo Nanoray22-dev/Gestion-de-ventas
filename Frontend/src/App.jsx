87397e22a2761be968ce2f6f33ba8885ab1ae2d8
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-02-28 13:57:27 -0600

    Resolviendo2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-// import SideBar from "./Components/SideBar";
 import Cards from "./Components/Dashboard/Cards";
-// import UsersList from "./Components/Gestion_usuarios/UsersList";
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Cards />
-      {/* <SideBar /> */}
-      {/* <UsersList /> */}
     </>
   );
 }
