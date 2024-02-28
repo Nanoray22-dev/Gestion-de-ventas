42c322811c6a4b34793498292c818358c962e1a9
Derek <adhoc607@gmail.com>
2024-02-28 11:39:24 -0500

    estilizacion de tabla de productos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import SideBar from "./Components/SideBar";
 import Cards from "./Components/Cards";
+import { TablaVentas } from "./Components/AgregarVentas/TablaVentas";
 
 function App() {
   return (
-    <div className="flex">
+    <>
+   {/*  <div className="flex">
       <SideBar />
       <Navbar />
       <div className="flex-1 p-8">
         <Cards />
       </div>
-    </div>
+    </div> */}
+    <TablaVentas/>
+    </>
   );
 }
 
--- Frontend/src/Components/AgregarVentas/Botones.jsx
+++ Frontend/src/Components/AgregarVentas/Botones.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export const Botones = ({iconText,
-  text}) => {
+export const Botones = ({ iconText, text }) => {
   return (
     <>
-    <a href="">
-    <span className="material-symbols-outlined">
-{iconText}
-</span>
-    <button>{text}</button>
-
-    </a>
+      <a href="" className="bg-gray-500 m-4 h-[20px] p-[8px] rounded-[5px] text-gray-300">
+        <span className="material-symbols-outlined ">{iconText}</span>
+        <button>{text}</button>
+      </a>
     </>
-  )
-}
+  );
+};
