d148e5aaf0938af250413d4a2804bb9d5adc9607
Derek <adhoc607@gmail.com>
2024-02-28 17:47:38 -0500

    tabla de ventas y botones terminados

--- Frontend/src/Components/AgregarVentas/Cantidad.jsx
+++ Frontend/src/Components/AgregarVentas/Cantidad.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-export const Cantidad = ({ descripcion, cantidad }) => {
+export const Cantidad = ({ descripcion, cantidad, iconText }) => {
   return (
     <>
-      <div>
-        <p>{descripcion}</p>
-        <span>{cantidad}</span>
+      <div className="text-gray-500 w-[170px] h-[30px] flex ">
+        <p className="px-3">{descripcion}</p>
+        <span className="material-symbols-outlined px-3 hover:text-red-400 hover:transition hover:duration-700">{iconText}</span>
+        <span className="text-black">{cantidad}</span>
       </div>
     </>
   );
