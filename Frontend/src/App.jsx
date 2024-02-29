1dd1036b42403a4d023a133c376dafe97f14dcab
Derek <adhoc607@gmail.com>
2024-02-29 10:04:52 -0500

    tabla para Listar Compras

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,20 +2,23 @@ import "./App.css";
 
 import Navbar from "./Components/Navbar/Navbar";
 import SideBar from "./Components/SideBar";
-import Cards from "./Components/Cards";
+/* import Cards from "./Components/Cards"; */
 import { TablaVentas } from "./Components/AgregarVentas/TablaVentas";
+import { TablaListarCompras } from "./Components/ListarCompras/TablaListarCompras";
 
 function App() {
   return (
     <>
-   {/*  <div className="flex">
-      <SideBar />
-      <Navbar />
-      <div className="flex-1 p-8">
+    <div className="flex">
+      {/* <SideBar />
+      <Navbar /> */}
+     {/*  <div className="flex-1 p-8">
         <Cards />
-      </div>
-    </div> */}
-    <TablaVentas/>
+      </div> */}
+    </div>
+    {/* <TablaVentas/> */}
+    <TablaListarCompras/>
+ 
     </>
 
 
---
+++ Frontend/src/Components/ListarCompras/TablaListarCompras.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Botones } from "../AgregarVentas/Botones";
+import UsersList from "../Gestion_usuarios/UsersList";
+
+export const TablaListarCompras = () => {
+  return (
+    <>
+    <div className="w-[85%] fl">
+    <div className="m-7">
+      <Botones iconText="add" text="Añadir Ventas" />
+      <Botones iconText="select_window" text="Ventas de Importación" />
+      </div>
+      <div className="flex justify-center gap-6 items-center">
+      <label htmlFor=""><strong>Elige una Fecha</strong></label>
+      <input type="date"  className="border border-gray-300 focus:outline-none w-[170px] h-[35px] px-3 mx-2 rounded-[5px]  bg-slate-100 hover:bg-gray-300 hover:text-white hover:transition hover:duration-700 hover:placeholder:text-white"/>
+      <p><strong>A</strong></p>
+      <input type="date"  className="border border-gray-300 focus:outline-none w-[170px] h-[35px] px-3 mx-2 rounded-[5px]  bg-slate-100 hover:bg-gray-300 hover:text-white hover:transition hover:duration-700 hover:placeholder:text-white"/>
+
+      </div>
+        <UsersList/>
+      </div>
+    </>
+  );
+};
