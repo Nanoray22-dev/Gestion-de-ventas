04cb7f65ad39886f5d68c51ed83c7a0d8cfbfb03
Nanoray22-dev <raysellconcepcionpaulino@gmail.com>
2024-03-04 15:58:28 -0400

    new change in dashboard

--- Backend/app/Http/Controllers/GastosController.php
+++ Backend/app/Http/Controllers/GastosController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class GastosController extends Controller
 {
     public function index()
+    {
 
         $gastos = Gastos::all();
         return response()->json($gastos);
