c40bb2221e00a2190a5049ed4a1a99aa51ae8ec0
omarlaneor <annielaneor@gmail.com>
2024-03-06 08:13:10 -0500

    Prueba para continuar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import React from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import SideBar from "./Components/SideBar";
-import Cards from "./Components/Cards";
+import { Routes, Route } from "react-router";
+import Navbar from "./Components/Navbar.jsx";
+import UserList from "./Components/Gestion_usuarios/UsersList.jsx";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
+import TablaVentas from "./Components/AgregarVentas/TablaVentas.jsx";
+import TablaListarCompras from "./Components/ListarCompras/TablaListarCompras.jsx";
+import Product from "./Components/AgregarVentas/Product.jsx";
+import ProductLists from "./Components/AgregarVentas/ProductLists.jsx";
 
 function App() {
   return (
-    <div className="flex">
-      <SideBar />
-      <Navbar />
-      <div className="flex-1 p-8">
-        <Cards />
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="/userlist" element={<UserList />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tablaventas" element={<TablaVentas />} />
+
+          <Route path="/tablalistarcompras" element={<TablaListarCompras />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,50 +1,177 @@
-import React, { useState } from "react";
 import { TextNavbar } from "./TextNavbar";
+import { Link, Outlet } from "react-router-dom";
+import {
+  HomeIcon,
+  ShoppingCartIcon,
+  CashIcon,
+  ChartBarIcon,
+  CurrencyDollarIcon,
+  UserAddIcon,
+  ChevronRightIcon,
+  CogIcon,
+  PlusSmIcon,
+} from "@heroicons/react/outline";
+
+import { useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <>
-      <nav className="bg-slate-100 p-[20px] text-gray-500 shadow-sm shadow-gray-300 top-0 fixed inset-x-64 right-0 z-10">
-        <ul className="flex items-center justify-between">
-          <div className="rounded-[5px] hover:transition hover:duration-700 hover:ease-in-out bg-slate-100">
-            <TextNavbar spanIcon="menu" text="" />
-          </div>
-          <div>
-            <img src="" alt="" />
-          </div>
-          <div className="flex">
+    <div className="grid-container">
+      {/* Header */}
+      <header className="header">
+        {/* Botón de menú para mostrar/ocultar el sidebar */}
+        <button className="menu-icon" onClick={toggleSidebar}>
+          <TextNavbar spanIcon="menu" text="" />
+        </button>
+        <div className="header-left list-none">
+          <TextNavbar spanIcon="menu" text="" />
+        </div>
+        <div className="flex list-none">
+          <Link to="/tablaventas">
             <TextNavbar spanIcon="shopping_bag" text="Ventas" />
-            <TextNavbar spanIcon="language" text="Idioma" />
-            <TextNavbar spanIcon="info" text="Ayuda" />
-            <div className="relative">
-              <TextNavbar
-                spanIcon="person"
-                text="Admin"
-                onClick={toggleDropdown}
-              />
-              {showDropdown && (
-                <div className="absolute right-0 mt-1 bg-white border border-gray-200 shadow-md rounded-md">
-                  <ul>
-                    <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer">
-                      Administrar Usuarios
-                    </li>
-                    <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer">
-                      Configuración
-                    </li>
-                  </ul>
-                </div>
-              )}
-            </div>
+          </Link>
+          <TextNavbar spanIcon="language" text="Idioma" />
+          <TextNavbar spanIcon="info" text="Ayuda" />
+
+          {/* User Dropdown */}
+          <div className="relative">
+            <TextNavbar
+              spanIcon="person"
+              text="Admin"
+              onClick={toggleDropdown}
+            />
+            {showDropdown && (
+              <div
+                className="absolute right-0 mt-1 bg-white border border-gray-200 shadow-md rounded-md"
+                onMouseLeave={() => setShowDropdown(false)}
+              >
+                <ul>
+                  <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer ">
+                    <Link to="/userlist">Administrar Usuarios</Link>
+                  </li>
+                  <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer">
+                    <Link to="/">ConfiguraciÓn</Link>
+                  </li>
+                  <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer">
+                    <Link to="/">Mis compras</Link>
+                  </li>
+                  <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer">
+                    <Link to="/">Mis ventas</Link>
+                  </li>
+                  <li className="py-1 px-4 hover:bg-gray-100 cursor-pointer">
+                    <Link to="/">Log out</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
+        </div>
+      </header>
+      {/* End Header */}
+
+      {/* Sidebar */}
+      <aside id="sidebar" className={sidebarOpen ? "sidebar-responsive" : ""}>
+        <div className="sidebar-title">
+          <div className="sidebar-brand">
+            <h2 className="text-3xl font-bold mb-8 text-center flex items-center">
+              <ChartBarIcon className="w-8 h-8 mr-1 ml-8" />
+              Tablero
+            </h2>
+          </div>
+          <span className="material-icons-outlined" onClick={toggleSidebar}>
+            close
+          </span>
+        </div>
+
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item">
+            <Link
+              to="/dashboard"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <HomeIcon className="w-6 h-6 mr-2" />
+              Inicio
+            </Link>
+          </li>
+          <li className="sidebar-list-item">
+            <Link
+              to="tablalistarcompras"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <ShoppingCartIcon className="w-6 h-6 mr-2" />
+              Compra
+              <ChevronRightIcon className="w-6 h-6 ml-20" />
+            </Link>
+          </li>
+          <li className="sidebar-list-item">
+            <Link
+              to="/userlist"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <UserAddIcon className="w-6 h-6 mr-2" />
+              Usuarios
+            </Link>
+          </li>
+          <li className="sidebar-list-item">
+            <Link
+              to="/tablaventas"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <CashIcon className="w-6 h-6 mr-2" />
+              Venta
+            </Link>
+          </li>
+          <li className="sidebar-list-item">
+            <Link
+              to="tablalistarcompras"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <PlusSmIcon className="w-6 h-6 mr-2" />
+              Productos
+            </Link>
+          </li>
+          <li className="sidebar-list-item">
+            <a
+              href="#"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <CurrencyDollarIcon className="w-6 h-6 mr-2" />
+              Gasto
+              <ChevronRightIcon className="w-6 h-6 ml-20" />
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a
+              href="#"
+              className="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-300"
+            >
+              <CogIcon className="w-6 h-6 mr-2" />
+              Settings
+            </a>
+          </li>
         </ul>
-      </nav>
-    </>
+      </aside>
+
+      {/* End Sidebar */}
+
+      {/* Main */}
+      <main className="main-container">
+        <Outlet />
+      </main>
+      {/* End Main */}
+    </div>
   );
 };
 
