42c3a318d86956b3a40d5514f46d173dd9087112
omarlaneor <annielaneor@gmail.com>
2024-03-07 10:11:13 -0500

    Reporte PDF Usuarios

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GananciasController;
 use App\Http\Controllers\GastosController;
 use App\Http\Controllers\IngresosController;
+use App\Http\Controllers\reporteUsuariosController;
 use App\Http\Controllers\usuariosController;
 use App\Http\Controllers\VentasController;
 use Illuminate\Http\Request;
@@ -47,8 +48,12 @@ Route::post('/ganancias', [GananciasController::class, 'store']);
 Route::put('/ganancias/{id}', [GananciasController::class, 'update']);
 Route::delete('/ganancias/{id}', [GananciasController::class, 'destroy']);
 
+
 Route::get('/ventas', [VentasController::class, 'index']);
 Route::get('/ventas/{id}', [VentasController::class, 'show']);
 Route::post('/ventas', [VentasController::class, 'store']);
 Route::put('/ventas/{id}', [VentasController::class, 'update']);
 Route::delete('/ventas/{id}', [VentasController::class, 'destroy']);
+
+Route::get('usuarios-pdf', [reporteUsuariosController::class, 'indexPdf']);
+
--- Frontend/src/Components/Gestion_usuarios/UsersList.jsx
+++ Frontend/src/Components/Gestion_usuarios/UsersList.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import Swal from "sweetalert2";
 import AddUserModal from "./AddUserModal";
 import EditUserModal from "./EditUserModal";
 import { Menu } from "@headlessui/react";
-
+import axios from "axios";
+import { endpoint } from "../../services/http";
 import ColumnVisibilityDropdown from "./ColumnVisibilityDropdown";
 
 function UsersList() {
@@ -17,6 +17,17 @@ function UsersList() {
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage, setUsersPerPage] = useState(10);
+
+  const getPdf = async () => {
+    const response = await axios.get(`${endpoint}/usuarios-pdf`, {
+      responseType: "blob",
+    });
+
+    const url = window.URL.createObjectURL(
+      new Blob([response.data], { type: "application/pdf" })
+    );
+    window.open(url, "_blank");
+  };
 
   useEffect(() => {
     fetchUsers();
@@ -255,7 +266,10 @@ function UsersList() {
             className="border rounded px-2 py-1"
           />
           <div className="flex items-center">
-            <button className="bg-gray-300 text-gray-700 rounded px-4 py-2 mr-2 transition duration-300 hover:bg-gray-500 hover:text-white">
+            <button
+              className="bg-gray-300 text-gray-700 rounded px-4 py-2 mr-2 transition duration-300 hover:bg-gray-500 hover:text-white"
+              onClick={getPdf}
+            >
               PDF
             </button>
             <button className="bg-gray-300 text-gray-700 rounded px-4 py-2 mr-2 transition duration-300 hover:bg-gray-500 hover:text-white">
